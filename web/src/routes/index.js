bef34c6cef36d9b46028377410f1d603f0977c62
Jhow Paes <jhow.paes@dotcode.com.br>
2019-10-09 14:34:10 -0300

    add private routes and default layout

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -13,9 +15,9 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/meetup" component={Meetup} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/meetup" component={Meetup} isPrivate />
     </Switch>
   );
 }
