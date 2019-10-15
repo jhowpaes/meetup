b3577db08947939231f8193a5d646c212964a3a8
Jhow Paes <jhow.paes@dotcode.com.br>
2019-10-15 10:20:29 -0300

    add profile page

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  name: Yup.string(),
+  email: Yup.string().email(),
+  oldPassword: Yup.string(),
+  password: Yup.string().when('oldPassword', (oldPassword, field) =>
+    oldPassword ? field.required('Old password is required') : field
+  ),
+  confirmPassword: Yup.string().when('password', (password, field) =>
+    password
+      ? field.required('incorrect password').oneOf([Yup.ref('password')])
+      : field
+  ),
+});
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your email address" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Current password"
+        />
+        <Input name="password" type="password" placeholder="New password" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm password"
+        />
+
+        <button type="submit">Save profile</button>
+      </Form>
+    </Container>
+  );
 }
---
+++ web/src/pages/Profile/styles.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 940px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 50px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+      font-size: 18px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+        opacity: 0.5;
+      }
+    }
+
+    span {
+      color: #f64c75;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 20px 0 20px;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 42px;
+      width: 162px;
+      background: #f94d6a;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, '#f94d6a')};
+      }
+    }
+  }
+`;
