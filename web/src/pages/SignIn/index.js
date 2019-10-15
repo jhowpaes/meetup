62fa8e4f46de5f649cc4883cc48c6d6c7cf43ec5
Jhow Paes <jhow.paes@dotcode.com.br>
2019-10-15 10:20:01 -0300

    add signup and signin

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,30 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
 const schema = Yup.object().shape({
   email: Yup.string()
     .email('E-mail invalido')
-    .required('Campo obrigatório'),
-  password: Yup.string().required('Campo obrigatório'),
+    .required('Email is required'),
+  password: Yup.string().required('Password is required'),
 });
 
 export default function SignIn() {
-  function handleSubmit(data) {}
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <>
       <img src={logo} alt="Meetapp" />
 
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input type="email" name="email" placeholder="Seu e-mail" />
-        <Input type="password" name="password" placeholder="Sua senha" />
+        <Input type="email" name="email" placeholder="Your email" />
+        <Input type="password" name="password" placeholder="Your password" />
 
-        <button type="submit">Entrar</button>
-        <Link to="/register">Criar conta grátis</Link>
+        <button type="submit">{loading ? '...Loading' : 'Login'}</button>
+        <Link to="/register">Create free account</Link>
       </Form>
     </>
   );
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 const schema = Yup.object().shape({
-  name: Yup.string().required('Campo obrigatório.'),
+  name: Yup.string().required('Name is required'),
   email: Yup.string()
-    .email('E-mail invalido')
-    .required('Campo obrigatório'),
+    .email('Invalid email')
+    .required('Email is required'),
   password: Yup.string()
-    .min(6, 'No mínimo 6 caracteres')
-    .required('Campo obrigatório'),
+    .min(6, '6 characters minimum')
+    .required('Password is required'),
+  confirmPassword: Yup.string().when('password', (password, field) =>
+    password
+      ? field.required('incorrect password').oneOf([Yup.ref('password')])
+      : field
+  ),
 });
 
 export default function SignUp() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
 
   return (
@@ -25,16 +35,17 @@ export default function SignUp() {
       <img src={logo} alt="Meetapp" />
 
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input type="text" name="name" placeholder="Nome completo" />
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="text" name="name" placeholder="Full name" />
+        <Input type="email" name="email" placeholder="Email" />
+        <Input type="password" name="password" placeholder="Password" />
         <Input
+          name="confirmPassword"
           type="password"
-          name="password"
-          placeholder="Sua senha secreta"
+          placeholder="Confirm password"
         />
 
-        <button type="submit">Criar conta</button>
-        <Link to="/">Já tenho login</Link>
+        <button type="submit">Create account</button>
+        <Link to="/">I already have a login</Link>
       </Form>
     </>
   );
